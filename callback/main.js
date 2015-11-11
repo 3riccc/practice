c32c0bc5e1090e07f07a5540f2a4daabea682a35
3riccc <ericinisa@163.com>
2015-11-11 16:44:07 +0800

    在prac.js里面写了一个神奇的可以给任意个数字求和的函数

---
+++ callback/main.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+	// function call (arg,callback) {
+	// 	console.log(arg);
+	// 	var arg1 = arg+4;
+	// 	callback(arg);
+	// }
+	// function call_1 (arg) {
+	// 	console.log('this is in callback'+'------'+arg);
+	// }
+
+	// call(5,call_1);
+
+}
+
---
+++ prac.js
@@ -0,0 +1,16 @@
+/**
+ * 竟然写出了一个可以加上任意数字的函数
+ * @param {[number]} arg 下次要加的数
+ */
+function add (arg) {
+	add.last = arg;
+	add.func = function(arg){
+		console.log(add.last+arg);
+		add.last += arg;
+		return add.func;
+	}
+	return add.func;
+}
+
+// 这样调用
+add(2)(1)(4)(3);
